3818f1a06bc99e477c2a92f8ada4bf9ba05ceadc
Muhammad Zubair <zubairlegend18@gmail.com>
2026-05-05 00:27:26 +0500

    feat: Add username field to Redis configuration

--- Moodify/backend/src/config/cache.js
+++ Moodify/backend/src/config/cache.js
@@ -3,6 +3,7 @@ const Redis = require('ioredis').default;
 const redis = new Redis({
     host: process.env.REDIS_HOST,
     port: process.env.REDIS_PORT,
+    username: '',
     password: process.env.REDIS_PASSWORD
 });
 
