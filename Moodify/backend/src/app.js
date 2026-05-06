948a18d6e492975c88dd6549038934900f0e4a97
Muhammad Zubair <zubairlegend18@gmail.com>
2026-05-06 11:37:57 +0500

    feat: Update CORS configuration to use environment variable for client URL

--- Moodify/backend/src/app.js
+++ Moodify/backend/src/app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const CookieParser = require('cookie-parser');
 const cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 app.use(CookieParser());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: process.env.CLIENT_URL || 'http://localhost:5173',
     credentials: true
 }));
 
@@ -18,5 +19,10 @@ app.use('/api/auth', AuthRoutes);
 app.use('/api/songs', SongRoutes);
 app.use('/api/mood-history', MoodHistoryRoutes);
 
+app.use(express.static(path.join(__dirname, '../public')));
+
+app.get('{*splat}', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'index.html'));
+});
 
 module.exports = app;
